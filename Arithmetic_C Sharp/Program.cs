b6ac000d9e509624815068e7479cbe7530ce7d7b
Vinkens <Vinkens@Vinkens.com>
2020-07-07 17:41:23 +0800

    init repo

---
+++ Arithmetic_C Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arithmetic_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
